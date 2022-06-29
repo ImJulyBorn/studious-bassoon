ca72da09ca45cb7fb8eb15bff22f04cfb8d792f1
ImJulyBorn <106574769+ImJulyBorn@users.noreply.github.com>
2022-06-29 14:13:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX = 0;
+noseY = 0;
+function preload() {
+    mustache = loadImage("https://i.postimg.cc/NGHk8Z1J/mustache.png");
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(mustache, noseX-43, noseY, 80, 30);
+}
+
+function take_snapshot() {
+    save('i-look-great-in-mustaches.png');
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = ");
+        console.log("nose y = ");
+    }
+}
+
